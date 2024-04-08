b81a1a88b093fe7e67033a7ad370befde6847c5a
Stas Topilnytskiy <stastopilnytskii@gmail.com>
2024-04-08 17:24:32 +0300

    backend part done

--- src/main/java/org/eventhub/main/config/SecurityConfig.java
+++ src/main/java/org/eventhub/main/config/SecurityConfig.java
@@ -60,20 +60,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/v1/auth/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authenticationProvider(authenticationProvider)
-//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//        return http.build();
-//
-//
-//    }
 }
