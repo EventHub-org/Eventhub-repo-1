6f1e56a663f58ad335bc4c6cfd202d5dc4ee8944
Stanislav Topilnytskyi <stastopilnytskii@gmail.com>
2024-04-09 16:50:39 +0300

    fixed push problem

--- EventHub_client/src/pages/Home/MyEvents/MyEvents.jsx
+++ EventHub_client/src/pages/Home/MyEvents/MyEvents.jsx
@@ -1,35 +1,31 @@
-import React, { useState } from 'react';
-import { CalendarOutlined } from '@ant-design/icons';
-import { RoundButton } from '../../../components/Buttons/RoundButton/roundButton';
-import styles from './MyEvents.module.css';
-import EventList from './MyEventList';
-import { useSearchParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { CalendarOutlined } from "@ant-design/icons";
+import { RoundButton } from "../../../components/Buttons/RoundButton/roundButton";
+import styles from "./MyEvents.module.css";
+import EventList from "./MyEventsList";
+import { useSearchParams } from "react-router-dom";
 
 const MyEvents = () => {
-    const [searchParams, setSearchParams] = useSearchParams('');
+  const [searchParams, setSearchParams] = useSearchParams("");
 
-    const handleButtonClose = async () => {
-        if (searchParams.get("my_events")){
-            setSearchParams("")
-        } else {
-            setSearchParams({my_events:"true"})
-        }
+  const handleButtonClose = async () => {
+    if (searchParams.get("my_events")) {
+      setSearchParams("");
+    } else {
+      setSearchParams({ my_events: "true" });
     }
+  };
 
-    return (
-        <div className={styles.filterContainer}>
-            <div className={styles.filterButtonContainer}>
-                <RoundButton
-                    onClick={handleButtonClose}
-                    icon={<CalendarOutlined />}
-                />
-            </div>
-            {searchParams.get("my_events") && 
-            <EventList
-            handleButtonClose={handleButtonClose}
-            />}
-        </div>
-    );
+  return (
+    <div className={styles.filterContainer}>
+      <div className={styles.filterButtonContainer}>
+        <RoundButton onClick={handleButtonClose} icon={<CalendarOutlined />} />
+      </div>
+      {searchParams.get("my_events") && (
+        <EventList handleButtonClose={handleButtonClose} />
+      )}
+    </div>
+  );
 };
 
 export default MyEvents;
