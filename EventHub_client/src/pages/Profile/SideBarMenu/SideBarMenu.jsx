5e843076117f2b842be5cc6f74f23478e9d2b766
nazarius_pr <protsnazar2004@gamil.com>
2024-04-09 17:39:50 +0300

    formatted

--- EventHub_client/src/pages/Profile/SideBarMenu/SideBarMenu.jsx
+++ EventHub_client/src/pages/Profile/SideBarMenu/SideBarMenu.jsx
@@ -1,31 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { FiUser } from "react-icons/fi";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { LuUsers } from "react-icons/lu";
 import { FiLogOut } from "react-icons/fi";
-import getIdFromToken from '../../../jwt/getIdFromToken'
-import styles from './SideBarMenu.module.css';
+import getIdFromToken from "../../../jwt/getIdFromToken";
+import styles from "./SideBarMenu.module.css";
 
+const SideBarMenu = () => {
+  const userId = getIdFromToken();
 
-const SideBarMenu = () =>{
-    const userId = getIdFromToken();
+  const options = [
+    { name: "Account", icon: <FiUser />, source: `/profile/${userId}/account` },
+    { name: "Events", icon: <HiOutlineLocationMarker />, source: "/" },
+    { name: "Friends", icon: <LuUsers /> },
+  ];
 
-    const options = [
-        { name: 'Account', icon: <FiUser />, source:`/profile/${userId}/account` },
-        { name: 'Events', icon: <HiOutlineLocationMarker />, source: '/'},
-        { name: 'Friends', icon: <LuUsers /> }
-    ];
-    
-    return(
-        <ul className={styles.SideBarMenu}>
-            {options.map((option) =>{
-                    return <li className={styles.Option}><Link className={styles.Link} key={option} to={option.source}>
-                        {option.icon} {option.name}
-                    </Link></li>
-                })}
-            <li className={styles.Option}><Link className={styles.Link} key='logout' to={'/login'}><FiLogOut /> Logout</Link></li>
-        </ul>
-    );
-}
+  return (
+    <ul className={styles.SideBarMenu}>
+      {options.map((option) => {
+        return (
+          <li className={styles.Option}>
+            <Link className={styles.Link} key={option} to={option.source}>
+              {option.icon} {option.name}
+            </Link>
+          </li>
+        );
+      })}
+      <li className={styles.Option}>
+        <Link className={styles.Link} key="logout" to={"/login"}>
+          <FiLogOut /> Logout
+        </Link>
+      </li>
+    </ul>
+  );
+};
 
-export default SideBarMenu
+export default SideBarMenu;
