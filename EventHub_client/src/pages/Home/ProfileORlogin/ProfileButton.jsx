25bed8cb273bfd658fe8231d6c36e034fa7550ee
kriperAsasin2007 <119786509+kriperAsasin2007@users.noreply.github.com>
2024-04-10 10:44:45 +0300

    Temporary hardcoded nick profile to profile on home

--- EventHub_client/src/pages/Home/ProfileORlogin/ProfileButton.jsx
+++ EventHub_client/src/pages/Home/ProfileORlogin/ProfileButton.jsx
@@ -1,19 +1,20 @@
 // MenuButton.jsx
-import React, { useState } from 'react';
-import { Dropdown, Menu } from 'antd';
-import useAuth from '../../../hooks/useAuth';
-import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
-import ProfileInfo from '../../../components/ProfileInfo/ProfileInfo';
-import styles from './Buttons.module.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Dropdown, Menu } from "antd";
+import useAuth from "../../../hooks/useAuth";
+import { UserOutlined, LogoutOutlined } from "@ant-design/icons";
+import ProfileInfo from "../../../components/ProfileInfo/ProfileInfo";
+import styles from "./Buttons.module.css";
 
 const MenuButton = () => {
   const { setAuth } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
 
   const handleMenuClick = (e) => {
-    if (e.key === 'profile') {
-      console.log('Profile clicked');
-    } else if (e.key === 'logout') {
+    if (e.key === "profile") {
+      console.log("Profile clicked");
+    } else if (e.key === "logout") {
       localStorage.removeItem("token");
       setAuth({});
     }
@@ -24,19 +25,24 @@ const MenuButton = () => {
     setIsOpen(!isOpen);
   };
 
-
-
   return (
-    <Dropdown overlay={
-      <Menu className={styles.customMenu} onClick={handleMenuClick}>
-        <Menu.Item icon={<UserOutlined />} key="profile">Profile</Menu.Item>
-        <Menu.Item icon={<LogoutOutlined />} key="logout">Log out</Menu.Item>
-      </Menu>}
-      trigger={['click']}
+    <Dropdown
+      overlay={
+        <Menu className={styles.customMenu} onClick={handleMenuClick}>
+          <Link to={`/profile/9a070957-1eb8-4c23-a5f7-d448168e7166/account`}>
+            <Menu.Item icon={<UserOutlined />} key="profile">
+              Profile
+            </Menu.Item>
+          </Link>
+          <Menu.Item icon={<LogoutOutlined />} key="logout">
+            Log out
+          </Menu.Item>
+        </Menu>
+      }
+      trigger={["click"]}
       visible={isOpen}
-      onVisibleChange={toggleMenu
-      }>
-
+      onVisibleChange={toggleMenu}
+    >
       <ProfileInfo
         nickname="Your Nickname"
         email="example@example.com"
