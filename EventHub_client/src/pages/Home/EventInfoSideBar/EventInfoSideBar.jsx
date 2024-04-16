c031e67dc8e1848595c1e490af764ab524f20ca1
kriperAsasin2007 <119786509+kriperAsasin2007@users.noreply.github.com>
2024-04-16 17:08:40 +0300

    Finished requests count

--- EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
+++ EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
@@ -269,7 +269,7 @@ const EventInfoSideBar = ({ ownerId, eventId }) => {
                     onClick={handleShowAllParticipants}
                     className={styles["show-more-participants-btn"]}
                   >
-                    {requests.length > 0 && (
+                    {userId === ownerId && requests.length > 0 && (
                       <div className={styles["requests-count-container"]}>
                         <RequestsCount requestsLength={requests.length} />
                       </div>
