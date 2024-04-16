85c24465fe1a2a5ee5a67b239111265a46bbfcb4
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-04-16 14:11:15 +0300

    Fix bug with username

--- src/main/java/org/eventhub/main/model/User.java
+++ src/main/java/org/eventhub/main/model/User.java
@@ -114,4 +114,8 @@ public class User implements UserDetails {
     public String getUsername() {
         return email;
     }
+
+    public String getNickname(){
+        return username;
+    }
 }
