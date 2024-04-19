3554540ac075d570912a54ad3792ca86fb25cb06
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-04-19 16:03:48 +0300

    Add bearer token to headers

--- EventHub_client/src/api/getUserInfo.js
+++ EventHub_client/src/api/getUserInfo.js
@@ -2,8 +2,10 @@ import axios from "./axios";
 
 export const getUserInfo = async () => {
   try {
+    const accessToken = localStorage.getItem("token");
     const authAxios = axios.create({
       headers: {
+        Authorization: `Bearer ${accessToken}`,
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "content-type",
         "Access-Control-Allow-Credentials": "true",
