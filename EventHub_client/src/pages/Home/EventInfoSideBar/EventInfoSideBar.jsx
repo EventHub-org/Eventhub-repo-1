e1987d2792fbc73790acde1e7916c8e2accfd383
kriperAsasin2007 <119786509+kriperAsasin2007@users.noreply.github.com>
2024-05-16 21:23:34 +0300

    Adjusted isLoading circle

--- EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
+++ EventHub_client/src/pages/Home/EventInfoSideBar/EventInfoSideBar.jsx
@@ -56,7 +56,7 @@ const EventInfoSideBar = () => {
 
   const [reloadList, setReloadList] = useState(false);
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [joinedParticipants, setJoinedParticipants] = useState(null);
 
@@ -82,6 +82,7 @@ const EventInfoSideBar = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         if (authenticated) {
           const userState = await getParticipantState(eventId);
           setUserState(userState.state);
@@ -98,7 +99,6 @@ const EventInfoSideBar = () => {
 
         const fullEventData = await getFullEventById(eventId);
         setEvent(fullEventData);
-        setIsLoading(false);
         setIsFull(
           fullEventData.max_participants === fullEventData.participant_count
         );
@@ -158,6 +158,8 @@ const EventInfoSideBar = () => {
           }
         }
         navigate("/");
+      } finally {
+        setIsLoading(false);
       }
     };
 
