a4c32df7546865362782aea311563bfdd49f98b3
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-05-19 23:55:57 +0300

    implement azure postgres sql

--- src/main/java/org/eventhub/main/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/org/eventhub/main/service/impl/RefreshTokenServiceImpl.java
@@ -30,7 +30,7 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
         RefreshToken refreshToken = RefreshToken.builder()
                 .user(user)
                 .token(UUID.randomUUID().toString())
-                .expiryDate(new Date(System.currentTimeMillis() + 1000 * 60 * 2))
+                .expiryDate(new Date(System.currentTimeMillis() + 1000 * 60 * 20))
                 .build();
         return refreshTokenRepository.save(refreshToken);
     }
