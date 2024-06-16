823325bef63b0bfe3ffc3f9a9aaea75f214addd8
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-06-16 22:21:45 +0300

    Update EventSpecification.java
    implement myEvent criteria for filter

--- src/main/java/org/eventhub/main/specification/EventSpecification.java
+++ src/main/java/org/eventhub/main/specification/EventSpecification.java
@@ -1,14 +1,26 @@
 package org.eventhub.main.specification;
 
+import jakarta.persistence.criteria.Join;
 import org.eventhub.main.model.Event;
+import org.eventhub.main.model.Participant;
+import org.eventhub.main.model.User;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.UUID;
 
 public class EventSpecification {
     private EventSpecification(){}
-    public static Specification<Event> findUserEvents(UUID uuid){
-        return null;
+    public static Specification<Event> findUserEvents(UUID userId){
+        return (root, query, builder) -> {
+            Join<Event, Participant> participantJoin = root.join("participants");
+            Join<Participant, User> userJoin = root.join("users");
+
+            return builder.and(
+              builder.equal(userJoin.get("id"), userId),
+              builder.isTrue(participantJoin.get("isApproved"))
+            );
+
+        };
     }
     public static Specification<Event> findJoinedEvents(UUID uuid){
         return null;
