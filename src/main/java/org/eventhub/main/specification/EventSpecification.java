7c4a332ed75ed85ac7135b4a4472d27c8b8cb693
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-06-16 22:07:20 +0300

    Update EventSpecification.java
    Implement the basic look of event specification

--- src/main/java/org/eventhub/main/specification/EventSpecification.java
+++ src/main/java/org/eventhub/main/specification/EventSpecification.java
@@ -1,4 +1,24 @@
 package org.eventhub.main.specification;
 
+import org.eventhub.main.model.Event;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.UUID;
+
 public class EventSpecification {
+    private EventSpecification(){}
+    public static Specification<Event> findUserEvents(UUID uuid){
+        return null;
+    }
+    public static Specification<Event> findJoinedEvents(UUID uuid){
+        return null;
+    }
+
+    public static Specification<Event> findPendingEvents(UUID uuid){
+        return null;
+    }
+
+    public static Specification<Event> findArchiveEvents(UUID uuid){
+        return null;
+    }
 }
