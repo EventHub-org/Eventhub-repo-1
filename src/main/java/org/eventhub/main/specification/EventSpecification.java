8e102234cc63ffcafc0862daea343bc7260d88bc
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-06-16 22:34:34 +0300

    Update EventSpecification.java
    implement archive events criteria for filter

--- src/main/java/org/eventhub/main/specification/EventSpecification.java
+++ src/main/java/org/eventhub/main/specification/EventSpecification.java
@@ -6,6 +6,7 @@ import org.eventhub.main.model.Participant;
 import org.eventhub.main.model.User;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class EventSpecification {
@@ -26,11 +27,24 @@ public class EventSpecification {
         };
     }
 
-    public static Specification<Event> findPendingEvents(UUID uuid){
-        return null;
+    public static Specification<Event> findPendingEvents(UUID userId){
+        return (root, query, builder) -> {
+            Join<Event, Participant> participantJoin = root.join("participants");
+            Join<Participant, User> userJoin = root.join("users");
+
+            return builder.and(
+                    builder.equal(userJoin.get("id"), userId),
+                    builder.isFalse(participantJoin.get("isApproved"))
+            );
+
+        };
     }
 
-    public static Specification<Event> findArchiveEvents(UUID uuid){
-        return null;
+    public static Specification<Event> findArchiveEvents(UUID userId){
+        LocalDateTime currentTime = LocalDateTime.now();
+        return (root, query,builder)-> builder.and(
+                builder.equal(root.get("owner"), userId),
+                builder.lessThanOrEqualTo(root.get("expireAt"), currentTime)
+        );
     }
 }
